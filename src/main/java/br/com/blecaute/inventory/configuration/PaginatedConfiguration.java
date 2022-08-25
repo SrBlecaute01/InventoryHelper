3b2589c1bae3a8233e4d16801ecf758d0576fc3d
SrBlecaute01 <josearnaldoma18k@gmail.com>
2022-08-25 00:34:44 -0300

    :bug: Fixing constructors to InventoryConfigurationBuilder.

--- src/main/java/br/com/blecaute/inventory/configuration/PaginatedConfiguration.java
+++ src/main/java/br/com/blecaute/inventory/configuration/PaginatedConfiguration.java
@@ -14,6 +14,7 @@ import java.util.*;
 @Getter
 @Builder(toBuilder = true)
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class PaginatedConfiguration implements Configuration {
 
     @NonNull
